e232449b6addec81dbda8d05f2bd207171fb2b34
MTPE <mtpe2301@gmail.com>
2024-11-24 20:14:14 +0700

    Dockerfile

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,30 @@
+// const mongoose = require('mongoose');
+
+// const mongoURIDev = process.env.MONGO_URI_DEV || 'mongodb://localhost:27017/ecommerce';
+// const mongoURIProd = process.env.MONGO_URI_PROD || 'mongodb+srv://user:user@cluster0.1c9llif.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0';
+
+// const connectDB = async () => {
+//   const mongoURI = process.env.NODE_ENV === 'production' ? mongoURIProd : mongoURIDev;
+
+//   try {
+//     await mongoose.connect(mongoURI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log(`MongoDB connected (${process.env.NODE_ENV === 'production' ? 'Production' : 'Development'})`);
+//   } catch (err) {
+//     console.error('MongoDB connection error:', err);
+//     process.exit(1); // Exit process on connection failure
+//   }
+// };
+
+// module.exports = connectDB;
+
 const mongoose = require('mongoose');
 
-const mongoURIDev = process.env.MONGO_URI_DEV || 'mongodb://localhost:27017/ecommerce';
-const mongoURIProd = process.env.MONGO_URI_PROD || 'mongodb+srv://user:user@cluster0.1c9llif.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0';
+const mongoURI = process.env.NODE_ENV === 'production' ? process.env.MONGO_URI_PROD : process.env.MONGO_URI_DEV;
 
 const connectDB = async () => {
-  const mongoURI = process.env.NODE_ENV === 'production' ? mongoURIProd : mongoURIDev;
-
   try {
     await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
